99096824c2135595a8332e286f8fb19cee302a23
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-07-24 10:40:49 +0300

    Completed Bank Transactions statements

--- my-project/src/SearchBar.jsx
+++ my-project/src/SearchBar.jsx
@@ -1,13 +1,15 @@
 // SearchBar.jsx
 import React from 'react';
 
+// SearchBar component for filtering transactions based on description
 const SearchBar = ({ searchTerm, setSearchTerm }) => {
   return (
+    // Input element to capture search input
     <input
       type="text"
-      value={searchTerm}
-      onChange={(e) => setSearchTerm(e.target.value)}
-      placeholder="Search by description"
+      value={searchTerm} // Set the value of the input field to the searchTerm state
+      onChange={(e) => setSearchTerm(e.target.value)} // Call setSearchTerm when the input value changes
+      placeholder="Search by description" // Placeholder text to guide users for what to search
     />
   );
 };
